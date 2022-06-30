87013d259616b3d86f517dacfee4b79d6a747dfd
zoer <zoer.aleksandr@gmail.com>
2022-06-30 19:45:01 +0300

    создал splash screen для Android 12

--- app/src/main/java/com/example/dictionary/view/MainActivity.kt
+++ app/src/main/java/com/example/dictionary/view/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.dictionary.view
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.dictionary.R
@@ -9,18 +16,25 @@ import com.example.dictionary.databinding.ActivityMainBinding
 import com.example.dictionary.view.favorite_screen.FavoriteListFragment
 import com.example.dictionary.view.history_screen.HistoryFragment
 import com.example.dictionary.view.main_search_screen.MainSearchFragment
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity(), Contract {
     private val binding: ActivityMainBinding by viewBinding()
+    private lateinit var splashScreen: SplashScreen
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen = installSplashScreen()
+            startSplash()
+        }
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(binding.container.id, MainSearchFragment())
                 .commitNow()
         }
+
         binding.bottomNavigation.setOnItemSelectedListener { menu ->
             when (menu.itemId) {
                 R.id.history -> {
@@ -60,5 +74,31 @@ class MainActivity : AppCompatActivity(), Contract {
             .replace(binding.container.id, fragment)
             .addToBackStack(null)
             .commit()
+    }
+
+    private fun startSplash() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setKeepOnScreenCondition { true }
+
+            Executors.newSingleThreadExecutor().execute {
+                Thread.sleep(1000)
+                splashScreen.setKeepOnScreenCondition { false }
+            }
+
+            splashScreen.setOnExitAnimationListener { provider ->
+                ObjectAnimator.ofFloat(
+                    provider.view,
+                    View.TRANSLATION_X,
+                    0f,
+                    -provider.view.width.toFloat()
+                ).apply {
+                    duration = 300
+                    interpolator = AnticipateInterpolator()
+                    doOnEnd {
+                        provider.remove()
+                    }
+                }.start()
+            }
+        }
     }
 }
